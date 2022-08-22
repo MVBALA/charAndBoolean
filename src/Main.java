2a3cfe6c65bb49467829ab841b78c0f4e667fa1e
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-22 18:34:50 +0530

    it is udemy exercise

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    char myChar='D';
+    char unicode='\u004A';
+        System.out.println("myChar = " + myChar);
+        System.out.println("unicode = " + unicode);
+        char copyrightChar='\u00A9';
+        System.out.println("copyrightChar = " + copyrightChar);
+        boolean my=true;
+        boolean ne=false;
+        System.out.println(" my = " +  my);
+        System.out.println("ne = " + ne);
     }
 }
--- src/academy/learnprograming/strings.java
+++ src/academy/learnprograming/strings.java
@@ -1,2 +1,34 @@
-package academy.learnprograming;public class strings {
-}
+package academy.learnprograming;
+
+public class strings {
+    public static void main(String[] args)
+    {
+        String k="This is ME ";
+        System.out.println("k = " + k);
+        k=k+ "and he is a learner";
+        System.out.println("k = " + k);
+        k=k+"\u00A9 2022";
+        System.out.println("k = " + k);
+        String cA="10";
+        int My=40;
+        cA=cA+My;
+        System.out.println("cA = " + cA);
+        float s=120.345f;
+        cA=cA+s;
+        System.out.println("cA = " + cA);
+        boolean a=true;
+        boolean b=a?true:false;
+        int t=5^6;
+       if (b)
+         //   System.out.println("it is not for me");
+          /*  System.out.println("b = " + b);
+            int st=87;
+            if (t>st || t<100)
+                System.out.println("st = " + st);
+            else
+        {*/
+            System.out.println("t = " + t);
+        //}
+
+
+}}
